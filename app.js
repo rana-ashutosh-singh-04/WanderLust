8856704c42d152ac442cd63a47448c28c68d0013
rana-ashutosh-singh-04 <singhranaashutosh6@gmail.com>
2025-07-22 13:20:08 +0530

    Error handelling through expresserror.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require('ejs-mate');
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
+const wrapAsync = require("./utils/wrapAsync.js")
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 main()
 .then(()=>{
@@ -32,51 +35,67 @@ app.get("/",(req,res)=>{
 })
 
 // index route
-app.get("/listings", async(req,res)=>{
+app.get("/listings", wrapAsync(async(req,res)=>{
    const allListings = await Listing.find({});
    res.render("./listings/index.ejs",{allListings})
-})
+}))
 
-// create route
+// create route : get
 app.get("/listings/new",(req,res)=>{
     res.render("listings/new.ejs")
 })
 
-app.post("/listings",async(req,res)=>{
+// create route : post
+app.post("/listings",
+    wrapAsync( async(req,res,next)=>{
+        // error handelling - invalid data inserting through hopscotch
+        // if(!req.body.listing){
+        //     throw new ExpressError(400, "send valid data for listings")
+        // }
     // let {title, description, image, price, country, location} = req.body;
+    let result = listingSchema.validate(req.body);
+    console.log(result);
+    if(result.error){
+        throw new ExpressError(404,result.error)
+    }
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-})
+    })
+)
 // show route
-app.get("/listings/:id",async(req,res)=>{
+app.get("/listings/:id", wrapAsync(async(req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing})
-})
+}))
 
 
 // edit route
-app.get("/listings/:id/edit", async (req,res)=>{
+app.get("/listings/:id/edit", wrapAsync(async (req,res)=>{
     let {id} = req.params;  
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs",{listing})
-})
+}))
 
 // update route
-app.put("/listings/:id", async(req,res)=>{
+app.put("/listings/:id", wrapAsync(async(req,res)=>{
+     if(!req.body.listing){
+            throw new ExpressError(400, "send valid data for listings")
+        }
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing})
     res.redirect(`/listings/${id}`)
-})
+}))
 
 // Destroy route
-app.delete("/listings/:id",async (req,res)=>{
+app.delete("/listings/:id",wrapAsync(async (req,res)=>{
     let {id} = req.params;
     const deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-})
+}))
+
 
 // app.get("/testListing",async (req,res)=>{
 //     let sampleListing = new Listing({
@@ -91,6 +110,17 @@ app.delete("/listings/:id",async (req,res)=>{
 //     console.log("sample was saved");
 //     res.send("successful testing")
 // })
+
+app.all("",(req,res,next)=>{
+    next(new ExpressError(404, "page not found!"))
+})
+
+app.use((err,req,res,next)=>{
+    
+    let {statusCode=500,message="Internal server error"} = err;
+    // res.status(statusCode).send(message);
+   res.render("error.ejs",{message})
+});
 
 app.listen(8080,()=>{
     console.log("server to the port 8080")
